b0cf862186e0c625020a1dc53324284aa6bef616
Exmar <polevdenis74@gmail.com>
2025-03-19 17:50:42 +0500

    Система редактирования, удаления, подтверждения удаления заметок

--- crm/views.py
+++ crm/views.py
@@ -3,6 +3,7 @@ from .forms import TaskForm, CommunicationForm, InvoiceForm, ProjectForm
 from django.http import HttpResponse
 from .models import Task, Message, Communication
 from django.http import JsonResponse
+from django.urls import reverse
 # Create your views here.
 
 def home(request):
@@ -33,37 +34,50 @@ def create_task(request):
 	return HttpResponse(status=400)
 
 def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)  
-    task.delete()  
-    return JsonResponse({'message': 'Задача удалена'}, status=200)
+	task = get_object_or_404(Task, id=task_id)
+	
+	if request.method == "POST":
+			task.delete()
+			return redirect('home')
+
+	return render(request, 'crm/delete_confirmation.html', {'task': task})
+
+def delete_communication(request, communication_id):
+	communication = get_object_or_404(Communication, id=communication_id)
+
+	if request.method == "POST":
+			task_id = communication.task.id  
+			communication.delete()
+			return redirect(reverse('task_message', args=[task_id]))
+
+	return render(request, 'crm/delete_communication.html', {'communication': communication})
 
 def task_detail(request, task_id):
 	task = get_object_or_404(Task, id=task_id)
 	return render(request, 'crm/task_detail.html', {'task': task})
 
 def task_message(request, task_id):
-	task = get_object_or_404(Task, id=task_id)
-	messages = Message.objects.filter(task=task)
-	communications = Communication.objects.filter(task=task).order_by('-created_at') 
-	
-	if request.method == 'POST':
-			form = CommunicationForm(request.POST, request.FILES)
-			if form.is_valid():
-					communication = form.save(commit=False)
-					communication.task = task
-					if not communication.tag and form.cleaned_data['custom_tag']:
-							communication.tag = form.cleaned_data['custom_tag']
-					communication.save()
-					return redirect('task_message', task_id=task.id)
-	else:
-			form = CommunicationForm()
-	
-	return render(request, 'crm/task_message.html', {
-			'task': task, 
-			'messages': messages,
-			'form': form,
-			'communications': communications, 
-	})
+    task = get_object_or_404(Task, id=task_id)
+
+    communications = Communication.objects.filter(task=task).order_by('-created_at')
+
+    if request.method == 'POST':
+        form = CommunicationForm(request.POST, request.FILES)
+        if form.is_valid():
+            communication = form.save(commit=False)
+            communication.task = task  # Привязываем заметку к задаче
+            if not communication.tag and form.cleaned_data['custom_tag']:
+                communication.tag = form.cleaned_data['custom_tag']
+            communication.save()
+            return redirect('task_message', task_id=task.id)
+    else:
+        form = CommunicationForm()
+
+    return render(request, 'crm/task_message.html', {
+        'task': task,
+        'form': form,
+        'communications': communications 
+    })
 
 def add_communication(request, task_id):
 	task = get_object_or_404(Task, id=task_id)
@@ -81,6 +95,37 @@ def add_communication(request, task_id):
 		form = CommunicationForm()
 
 	return render(request, 'crm/add_communication.html', {'form':form, 'task':task})
+
+def edit_message(request, message_id):
+	message = get_object_or_404(Communication, id=message_id)
+
+	if request.method == 'POST':
+		form = CommunicationForm(request.POST, request.FILES, instance=message)
+		if form.is_valid():
+			form.save()
+			return redirect('task_message', task_id=message.task.id)
+		else:
+			form = CommunicationForm(instance=message)
+		return render(request, 'crm/edit_message.html', {
+			'form': form,
+			'message': message
+    })
+	
+def edit_communication(request, communication_id):
+	communication = get_object_or_404(Communication, id=communication_id)
+
+	if request.method == 'POST':
+			form = CommunicationForm(request.POST, request.FILES, instance=communication)
+			if form.is_valid():
+					form.save()
+					return redirect('task_message', task_id=communication.task.id)
+	else:
+			form = CommunicationForm(instance=communication)
+
+	return render(request, 'crm/edit_communication.html', {
+			'form': form,
+			'communication': communication
+	})
 
 
 
--- crm_project/urls.py
+++ crm_project/urls.py
@@ -23,10 +23,23 @@ urlpatterns = [
 	
     #Home
 	path('',  views.home, name='home'),
+	
 	path('create-project/', views.create_project, name='create_project'),
+	
 	path('create_task/', views.create_task, name='create_task'),
-	path('delete-task/<int:task_id>/', views.delete_task, name='delete_task'),
+	
+	path('task/<int:task_id>/delete/', views.delete_task, name='delete_task'),
+	
 	path('task-details/<int:task_id>/', views.task_detail, name='task_detail'),
+	
 	path('task-message/<int:task_id>/', views.task_message, name='task_message'),
-	 path('task/<int:task_id>/add-communication/', views.add_communication, name='add_communication'),
+	
+	path('task/<int:task_id>/add-communication/', views.add_communication, name='add_communication'),
+	
+	path('edit-message/<int:message_id>/', views.edit_message, name='edit_message'),
+	
+    path('communication/<int:communication_id>/edit/', views.edit_communication, name='edit_communication'),
+		
+    path('communication/<int:communication_id>/delete/', views.delete_communication, name='delete_communication'),
+	 
 ]
